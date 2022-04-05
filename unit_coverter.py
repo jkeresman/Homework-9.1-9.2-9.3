05cd5823c4787062533e06024eb98e037998138e
Josip Keresman <josipkeresman@gmail.com>
2022-04-05 22:56:36 +0200

    commit

--- unit_coverter.py
+++ unit_coverter.py
@@ -1,9 +1,44 @@
 def covert_km_miles(km):
+
     return km * 0.621371192
 
 
-while True:
+def kilometers_input():
 
     while True:
         try:
-            kilometers = float(input("Enter "))
+            km = float(input("Enter a number in kilometres:  "))
+            break
+        except ValueError:
+            print("Wrong input please try again!!")
+    return km
+
+
+def another_conversion():
+
+    flag = True
+
+    while True:
+        answer = input("Do you want to do another conversion [yes/no] ?  ")
+        if answer.lower() == "no" or answer.lower == "yes":
+            flag = False
+            break
+        elif answer.lower() == "yes":
+            print("I'm glad to hear that, enter required parameters again!!")
+            break
+        else:
+            print("Wrong input!!")
+
+    return flag
+
+
+print("Hello there , function of this program is to convert kilometers to miles\n")
+
+again_flag = True
+
+while again_flag:
+
+    kilometers = kilometers_input()
+    miles = covert_km_miles(kilometers)
+    print(f"{kilometers} km -> {miles} miles")
+    again_flag = another_conversion()
